7bfeeb141e7a0cac730bc4e0c7a539fee7e71b52
tmahind <tmahind@iu.edu>
2022-09-05 12:54:46 -0400

    init

---
+++ arrange_pichus.py
@@ -0,0 +1,62 @@
+#!/usr/local/bin/python3
+#
+# arrange_pichus.py : arrange agents on a grid, avoiding conflicts
+#
+# Submitted by : [PUT YOUR NAME AND USERNAME HERE]
+#
+# Based on skeleton code in CSCI B551, Fall 2022.
+
+import sys
+
+# Parse the map from a given filename
+def parse_map(filename):
+	with open(filename, "r") as f:
+		return [[char for char in line] for line in f.read().rstrip("\n").split("\n")][3:]
+
+# Count total # of pichus on house_map
+def count_pichus(house_map):
+    return sum([ row.count('p') for row in house_map ] )
+
+# Return a string with the house_map rendered in a human-pichuly format
+def printable_house_map(house_map):
+    return "\n".join(["".join(row) for row in house_map])
+
+# Add a pichu to the house_map at the given position, and return a new house_map (doesn't change original)
+def add_pichu(house_map, row, col):
+    return house_map[0:row] + [house_map[row][0:col] + ['p',] + house_map[row][col+1:]] + house_map[row+1:]
+
+# Get list of successors of given house_map state
+def successors(house_map):
+    return [ add_pichu(house_map, r, c) for r in range(0, len(house_map)) for c in range(0,len(house_map[0])) if house_map[r][c] == '.' ]
+
+# check if house_map is a goal state
+def is_goal(house_map, k):
+    return count_pichus(house_map) == k 
+
+# Arrange agents on the map
+#
+# This function MUST take two parameters as input -- the house map and the value k --
+# and return a tuple of the form (new_house_map, success), where:
+# - new_house_map is a new version of the map with k agents,
+# - success is True if a solution was found, and False otherwise.
+#
+def solve(initial_house_map,k):
+    fringe = [initial_house_map]
+    while len(fringe) > 0:
+        for new_house_map in successors( fringe.pop() ):
+            if is_goal(new_house_map,k):
+                return(new_house_map,True)
+            fringe.append(new_house_map)
+
+# Main Function
+if __name__ == "__main__":
+    house_map=parse_map(sys.argv[1])
+    print(house_map)
+    # This is k, the number of agents
+    k = int(sys.argv[2])
+    # print ("Starting from initial house map:\n" + printable_house_map(house_map) + "\n\nLooking for solution...\n")
+    # solution = solve(house_map,k)
+    # print ("Here's what we found:")
+    # print (printable_house_map(solution[0]) if solution[1] else "False")
+
+
---
+++ route_pichu.py
@@ -0,0 +1,58 @@
+#!/usr/local/bin/python3
+#
+# route_pichu.py : a maze solver
+#
+# Submitted by : [PUT YOUR NAME AND USERNAME HERE]
+#
+# Based on skeleton code provided in CSCI B551, Fall 2022.
+
+import sys
+
+# Parse the map from a given filename
+def parse_map(filename):
+        with open(filename, "r") as f:
+                return [[char for char in line] for line in f.read().rstrip("\n").split("\n")][3:]
+                
+# Check if a row,col index pair is on the map
+def valid_index(pos, n, m):
+        return 0 <= pos[0] < n  and 0 <= pos[1] < m
+
+# Find the possible moves from position (row, col)
+def moves(map, row, col):
+        moves=((row+1,col), (row-1,col), (row,col-1), (row,col+1))
+
+        # Return only moves that are within the house_map and legal (i.e. go through open space ".")
+        return [ move for move in moves if valid_index(move, len(map), len(map[0])) and (map[move[0]][move[1]] in ".@" ) ]
+
+# Perform search on the map
+#
+# This function MUST take a single parameter as input -- the house map --
+# and return a tuple of the form (move_count, move_string), where:
+# - move_count is the number of moves required to navigate from start to finish, or -1
+#    if no such route exists
+# - move_string is a string indicating the path, consisting of U, L, R, and D characters
+#    (for up, left, right, and down)
+
+def search(house_map):
+        # Find pichu start position
+        pichu_loc=[(row_i,col_i) for col_i in range(len(house_map[0])) for row_i in range(len(house_map)) if house_map[row_i][col_i]=="p"][0]
+        fringe=[(pichu_loc,0)]
+
+        while fringe:
+                (curr_move, curr_dist)=fringe.pop()
+                for move in moves(house_map, *curr_move):
+                        if house_map[move[0]][move[1]]=="@":
+                                return (7, 'DDDDDDD')  # return a dummy answer
+                        else:
+                                fringe.append((move, curr_dist + 1))
+
+# Main Function
+if __name__ == "__main__":
+        house_map=parse_map(sys.argv[1])
+        x = moves(house_map,2,0)
+        print(x)
+        # print("Shhhh... quiet while I navigate!")
+        # solution = search(house_map)
+        # print("Here's the solution I found:")
+        # print(str(solution[0]) + " " + solution[1])
+
